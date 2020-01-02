d306d46ff535c0726ab3939c82b2b0d90632ea89
chandanareddy039 <59171693+chandanareddy039@users.noreply.github.com>
2020-01-02 11:58:44 +0530

    Add files via upload

---
+++ main (12).c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,count;
+printf("\n Enter The Number:");
+scanf("%d",&n);
+while(n>0)
+{
+     count=count+n%10;
+    n=n/10;
+}
+printf("no of digits=%d",count);
+}
