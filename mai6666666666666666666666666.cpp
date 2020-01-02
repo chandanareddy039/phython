5a1783e54b371f9ad0b9760fcb0e1f6dc6ca7bdb
chandanareddy039 <59171693+chandanareddy039@users.noreply.github.com>
2020-01-02 10:08:38 +0530

    Add files via upload

---
+++ mai6666666666666666666666666.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    printf("enter the alphabet");
+    scanf("%c",&ch);
+switch(ch)
+{
+    case 'a':
+         printf("vowel");
+         break;
+    case 'e':
+         printf("vowel");
+         break;
+    case 'i':
+         printf("vowel");
+         break;
+         case 'o':
+         printf("vowel");
+         break;
+    case 'u':
+         printf("vowel");
+         break;
+         default:
+         printf("consonant");
+         break;}}
